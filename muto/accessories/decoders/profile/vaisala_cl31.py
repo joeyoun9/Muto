b15937d0f71c19d6acec26e468a6df6e62798818
jsyoung <joeyoungwx@gmail.com>
2013-02-10 11:18:21 -0600

    Updated comments and modified CL31 decoder to make it work more accurately.

--- muto/accessories/decoders/profile/vaisala_cl31.py
+++ muto/accessories/decoders/profile/vaisala_cl31.py
@@ -27,55 +27,55 @@ def read(ob):
             information, and status information. It is possible to get more
             information from these lines with a revision of this code.
     '''
-    OB_LENGTH = 770 #FIXME - the current return length is limited to 770
+    OB_LENGTH = 770  # FIXME - the current return length is limited to 770
     SCALING_FACTOR = 1.0e9
     'break the full ob text into it\'s constituent parts'
     p1 = ob.split(unichr(002))
     p2 = p1[1].split(unichr(003))
     code = p1[0].strip()
-    ob = p2[0].strip() # just contents between B and C
-    #unused currently checksum = p2[1].strip()
+    ob = p2[0].strip()  # just contents between B and C
+    # unused currently checksum = p2[1].strip()
 
-    data = ob.split("\n") # split into lines
+    data = ob.split("\n")  # split into lines
 
     'the last line of the profile should be the data line'
     prof = data[-1].strip()
     'grab status lines'
     sl1 = data[0].strip()
-    sl2 = data[-2].strip() # I will skip any intermediate data lines...
+    sl2 = data[-2].strip()  # I will skip any intermediate data lines...
 
-    status = np.array([sl1[0].replace('/','0'),
-                    sl1[1].replace('A','2').replace('W','1')]+
-                    sl1[2:-13].replace('/','0').split() + sl2[:-14].split(),
+    status = np.array([sl1[0].replace('/', '0'),
+                    sl1[1].replace('A', '2').replace('W', '1')] +
+                    sl1[2:-13].replace('/', '0').split() + sl2[:-14].split(),
                     dtype=np.float32)
     'status should have a length of 13... we shall see...'
     # determine height difference by reading the last digit of the code
-    height_codes = [0,10,20,5,5] #'0' is not a valid key, and will not happen
-    data_lengths = [0,770,385,1500,770]
+    height_codes = [0, 10, 20, 5, 5]  # '0' is not a valid key, and will not happen
+    data_lengths = [0, 770, 385, 1500, 770]
     'length between 770 and 1500'
     datLen = data_lengths[int(code[-1])]
     htMult = height_codes[int(code[-1])]
-    values = np.zeros(datLen,dtype=np.float32)
+    values = np.zeros(datLen, dtype=np.float32)
     ky = 0
-    for i in xrange(0,len(prof),5):
+    for i in xrange(0, len(prof), 5):
 
-        ven = prof[i:i+5]
+        ven = prof[i:i + 5]
         if ven[0:2] == "ff" or ven == '00000':
             # logic: ff corresponds to >=ff000, which is ~1e6, which is beyond super high
             values[ky] = 1
         else:
-            values[ky] = int(ven,16) # scaled to 100000sr/km (x1e9 sr/m)FYI
-        ky += 1 # keep the key up to date
+            values[ky] = int(ven, 16)  # scaled to 100000sr/km (x1e9 sr/m)FYI
+        ky += 1  # keep the key up to date
 
     # then the storage will be log10'd values
     out = {
-        'height':np.arange(0,10000,htMult)[:OB_LENGTH],
-        'bs':np.log10(values[:OB_LENGTH]/SCALING_FACTOR),
+        'height':np.arange(0, 10000, htMult)[:OB_LENGTH],
+        'bs':np.log10(values[:OB_LENGTH] / SCALING_FACTOR),
         'status':status
         }
     return out
 
-def decode_hex_string(string, fail_value=1):
+def decode_hex_string(string, fail_value=1, char_count=5):
     '''
     This is a compacted code for simply reading the hexadecimal string provided
     which comes from a CL31, or 51 message. It cannot compensate for
@@ -86,6 +86,12 @@ def decode_hex_string(string, fail_value=1):
     string: str
         the newline/index free string which will be read for individual 5-digit
         hexadeceimal values. Standard extreme-value logic is applied.
+        
+    fail_value: int, optional
+        the value assigned to supposedly unrealistic values
+        
+    char_count: int, optional
+        the number of characters per value in the string. For a CL31 this is 5
 
     Returns
     -------
@@ -94,17 +100,16 @@ def decode_hex_string(string, fail_value=1):
     value if you wish it to be something else. Negative values will incur NaNs.
     '''
     data_len = len(string)
-    data = np.zeros(1+data_len/5,dtype=int)
+    data = np.zeros(data_len / 5, dtype=int)
     key = 0
-    for i in xrange(0,data_len,5):
-        temp = string[i:i+5]
+    for i in xrange(0, data_len, 5):
+        temp = string[i:i + 5]
         if temp[0:2] == "ff" or temp == '00000':
             # logic: ff corresponds to >=ff000, which is ~1e6, which is preposterous
             data[key] = fail_value
         else:
-            data[key] = int(temp,16) # scaled to 100000sr/km (x1e9 sr/m)FYI
-            
-        key += 1 # keep the key up to date
+            data[key] = int(temp, 16)  # scaled to 100000sr/km (x1e9 sr/m)FYI
+
+        key += 1  # keep the key up to date
     return data
-    
-    
+
--- muto/storage/h5.py
+++ muto/storage/h5.py
@@ -7,7 +7,7 @@ HDF5 data files. Attempts to be generic, but probably fails at this point.
 Much of this is based off my thesis work, except completely using tables 
 instead of v arrays.
 '''
-import tables 
+import tables
 import fcntl
 import muto
 import numpy as np
@@ -28,7 +28,7 @@ class h5(object):
         the HDF5 document
         
         Parameters
-        ----------    
+        ----------
         fname : str
             String location of HDF5 source file (.h5,hdf5,etc...)
             
@@ -36,16 +36,17 @@ class h5(object):
         self.filename = fname
         self.doc = NullDoc()
 
-    def create(self, close=True, clear=False, indices=False, group='/', **variables):
-        # create an hdf5 table for use with datasets, ideally of known size...
-        # variables specifies what variables will be filled in this archive
+    def create(self, close=True, clear=False, indices=False, group='/',
+               **variables):
         """
         Create an HDF5 document formatted for the provided variables
         
         Parameters
         ----------
-            close : bool
-                tell if the file should be returned or closed
+            close : bool, optional (default=True)
+                tell if the file should be returned (left open) or closed
+            clear: bool, optional (default=False)
+                
             indexes : dict
                 A dict of {'name':[length (integer),]} values to save as 
                 single valued indices (Carrays)
@@ -56,37 +57,32 @@ class h5(object):
                 name=[length,length,...] values to state the expandable 
                 variables for the dataset
         """
-        
-        #FIXME  - only one category available.??
+
         if clear:
-            'if the doc has been opened before, then reopen it for appending.'
+            # then force the document open with write permissions
             self.doc, self.lock = h5openw(self.filename)
         else:
             self.doc, self.lock = h5opena(self.filename)
-            
-        'Create the group the data is going to sit in'
+
+        # Create the group the data is going to sit in'
         if group  is not '/':
             if '/' not in group:
                 group = group + '/'
             gp = group.split('/')
-            'then create the group'
             drop = '/'.join(gp[:-1]) + '/'
             try:
                 self.doc.createGroup(drop, name=gp[-1])
             except:
-                'in this case, the group already exists'
+                # Assume the group already exists.
                 pass
-        'Lets see if there is a way to clear the group soon'
-        
-        
-        
-        'Identify the compression filters we are going to want to use'
-        filters = tables.Filters(complevel=6, complib='zlib')#blosc
+        # 'Identify the compression filters we are going to want to use'
+        filters = tables.Filters(complevel=6, complib='zlib')
+        # Using BLOSC Compression, which is the default in newer builds.
         '''
         for now, index information is expected to be fixed, or else it should 
         be a variable, so I will leave it as a Carray.
         '''
-        
+
         if indices:
             for k in indices:
                 if type(indices[k]) == tuple:
@@ -95,16 +91,16 @@ class h5(object):
                         dims = dims + (i,)
                 else:
                     dims = (indices[k],)
-                    
+
                 self.doc.createCArray(group, k,
                                       tables.Float32Atom(), dims,
                                       filters=filters.copy())
-        '''
-        By reading the variable information, we can create the description
-        for this data table. 
-        '''
+
+        # By reading the variable information, we can create the description
+        # for this data table.
+
         table_description = {'time': tables.Int32Col(pos=1)}
-        'define the starting variable position'
+        # 'define the starting variable position'
         i = 2
         for k in variables:
             'data shape is equivalent to all the additional dimensions'
@@ -115,17 +111,16 @@ class h5(object):
         self.doc.createTable(group, 'data', table_description,
                                     filters=filters.copy())
         l.info('table created')
-        'NOTE -- EXPECTED ROWS HAS BEEN REMOVED!!!'
-        
-        'Set file attributes'        
+
+        # Set file attributes
         self.doc.setNodeAttr('/', 'creator', 'Muto v' + muto.version())
         self.doc.setNodeAttr('/', 'version', '1.3')
-        'set any group attributes. - a list of available attributes'
+        # Set any group attributes.
         if indices:
             self.doc.setNodeAttr(group, 'indices', indices.keys())
-        'now we need to add the time values as a CS index'
+        # now we need to add the time values as a CS index
         self.doc.getNode(group).data.cols.time.createCSIndex(filters=filters.copy())
-        'and instruct the table to auto-index'
+        # and instruct the table to auto-index
         self.doc.getNode(group).data.autoIndex = True
         if close:
             self.doc.close()
@@ -184,38 +179,40 @@ class h5(object):
         """
         if not self.doc or not self.doc.isopen:
             self.doc, self.lock = h5openr(self.filename)
-        'Determine specified time limits'
+        # Determine specified time limits
         table = self.doc.getNode(group).data
         if timetup:
             begin = timetup[0]
             end = timetup[1]
         elif duration and not end and not begin:
-            'assuming that the max time is in the last 100'
-            end = self.end(group,True)
+            end = self.end(group, True)
             begin = end - duration
         elif duration and begin:
             end = begin + duration
         elif duration and end:
             begin = end - duration
         elif not duration and not begin and not end:
-            # you gave me nothing
-            raise Exception('You must specify a time tuple (timetup), begin/end'\
+            # No indicator of begin/end was given
+            raise Exception('You must specify a time tuple (timetup), '\
+                            + 'begin/end'\
                             + ' or a duration in order to slice. Use dump() so see'\
-                            + ' an entire dataset') 
+                            + ' an entire dataset')
         out = {}
         if type(variables) == str:
             'Only one variable is requested, so we can use a prebuilt hack'
             try:
                 varlen = table[-1][variables].shape[0]
             except:
-                'This probably means the variable had no shape'
+                # This probably means the variable had no shape
                 varlen = 1
-            
+
             'a quick hack to make the most frequent requests faster'
-            out = np.array([(r['time'], r[variables]) for r in table.where('(time >= ' + str(begin) + ') & (time <= ' + str(end) + ')')],
+            out = np.array([(r['time'], r[variables])
+                            for r in table.where('(time >= ' + str(begin) \
+                                                 + ') & (time <= ' + str(end) \
+                                                 + ')')],
                            dtype=[('time', float), (variables, 'f4', (varlen,))])
-            'IN THE FUTURE, this can be modified to account for all the variables '
-        
+            # FIXME - modify this for all variables.
         elif True:
             'sneaky way to evade that else'
             'We are going to create a dtype structured array string'
@@ -231,7 +228,7 @@ class h5(object):
             'the for notation and tuple call do not seem to add monstrous overhead so far...'
             out = np.array([tuple([r[x] for x in variables]) for r in table.where('(time >= ' + str(begin) + ') & (time <= ' + str(end) + ')')],
                            dtype=dtype)
-            
+
         else:
             result = self.doc.getNode(group).data.getWhereList('(time >= ' + str(begin) + ')&(time <= ' + str(end) + ')')
             if len(result['time']) > 0:
@@ -252,17 +249,17 @@ class h5(object):
                 raise Exception('This data set does not have any data within the'\
                                 + ' times specified')
 
-        
+
         if not type(indices) == bool:
             if type(indices) == str:
-                indices = [indices] 
+                indices = [indices]
             for i in indices:
-                out[i] = self.doc.getNode(group, name=i)[:] 
+                out[i] = self.doc.getNode(group, name=i)[:]
 
         if not persist:
             self.doc.close()
         return out
-    
+
     def end(self, group='/', persist=False):
         '''
             Return the maximum time in the file as would be used if 
@@ -276,7 +273,7 @@ class h5(object):
         if not persist:
             self.doc.close()
         return max
-    
+
     def direct_r(self, group='/'):
         '''
             return direct access to the table with read-only access
@@ -284,7 +281,7 @@ class h5(object):
         if not self.doc or not self.doc.isopen:
             self.doc, self.lock = h5openr(self.filename)
         return self.doc.getNode(group).data
-    
+
     def direct_a(self, group='/'):
         '''
         directly access the data table with append access
@@ -293,7 +290,7 @@ class h5(object):
             self.doc, self.lock = h5opena(self.filename)
         return self.doc.getNode(group).data
 
-    def get_index(self, index, group='/'): # DEPRECATED
+    def get_index(self, index, group='/'):
         """
         Grab the value of a specific index from the document/group
         
@@ -306,14 +303,13 @@ class h5(object):
         """
         if not self.doc or not self.doc.isopen:
             self.doc, self.lock = h5openr(self.filename)
-        print 'WARNING - THIS IS A DEPRECATED FUNCTION'
         return self.doc.getNode(group, name=index)[0]
-        # take the first value ([0]) because indices are time invariant in that 
-        #dimension
-        
+        # take the first value ([0]) because indices are time invariant in that
+        # dimension
+
     def stat(self):
         '''
-        Quickly print the file object to stdout
+        A simple operation to print the file information to the terminal
         '''
         if not self.doc or not self.doc.isopen:
             self.doc, self.lock = h5openr(self.filename)
@@ -338,7 +334,7 @@ class h5(object):
             self.doc, self.lock = h5opena(self.filename)
 
         for i in indices:
-            self.doc.getNode(group, name=i)[:] = indices[i] # that is all!
+            self.doc.getNode(group, name=i)[:] = indices[i]  # that is all!
 
         self.close()
 
@@ -386,7 +382,7 @@ class h5(object):
 
         if not self.doc or not self.doc.isopen:
             self.doc, self.lock = h5opena(self.filename)
-        
+
         if not filter(self.doc, time, data):
             'Then the append does not pass their test, and should end'
             return False
@@ -400,12 +396,12 @@ class h5(object):
             row[v] = data[v]
         'Or this might be where it fails'
         row.append()
-        
+
 
         if not persist:
             self.doc.close()
         return True
-    
+
     def flush(self, group='/'):
         '''
         Flush the table 'data' from the group identified
@@ -414,7 +410,7 @@ class h5(object):
             self.doc, self.lock = h5opena(self.filename)
         self.doc.getNode(group).data.flush()
         self.close()
-        
+
     def index(self, group='/'):
         '''
         Flush the table 'data' from the group identified
@@ -423,7 +419,7 @@ class h5(object):
             self.doc, self.lock = h5opena(self.filename)
         self.doc.getNode(group).data.reIndex()
         self.close()
-        
+
     def dirty_index(self, group='/'):
         '''
         Flush the table 'data' from the group identified
@@ -432,7 +428,7 @@ class h5(object):
             self.doc, self.lock = h5opena(self.filename)
         self.doc.getNode(group).data.reIndexDirty()
         self.close()
-        
+
 
     def dump(self, variable, group='/'):
         """
@@ -463,22 +459,25 @@ class h5(object):
         return out
 
     def close(self):
-        
+
         if self.doc.isopen:
             self.doc.close()
 
-'create the null document object class so the isopen property can be assigned'
 class NullDoc(object):
+    '''
+    Initiate the document object within the h5 class, in a manner which
+    can be easily expanded when necessary. 
+    '''
     def __init__(self):
         self.isopen = False
 
 
-"These functions can have the lockout functionality applied to them later"
+# These functions can have the lockout functionality applied to them later
 def h5openr(f):
     lkf = False
     f = tables.openFile(f, mode='r')
     'and lock the file'
-    #FIXME - this locking does not seem to be effective
+    # FIXME - this locking does not seem to be effective
     return f, lkf
 
 def h5opena(f):
@@ -489,7 +488,7 @@ def h5opena(f):
 
 def h5openw(f):
     lkf = False
-    #this will destroy whatever file it is opening, note
+    # this will destroy whatever file it is opening, note
     f = tables.openFile(f, mode='w', title='ms')
     return f, lkf
 
